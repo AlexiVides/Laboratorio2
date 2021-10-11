db082005534b62187fb0e1e6ce5893d3d3655d34
AlexiVides <88692360+AlexiVides@users.noreply.github.com>
2021-10-11 15:17:15 -0600

    Repository

--- Laboratorio2/Laboratorio2/Reposity/ClsRPersona.cs
+++ Laboratorio2/Laboratorio2/Reposity/ClsRPersona.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using Laboratorio2.Data;
+using Laboratorio2.Service;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Laboratorio2.Reposity
 {
-    class ClsRPersona
+    class ClsRPersona : IPersona
     {
+        public void Guardar(persona persona)
+        {
+            using (almacendepersonasEntities ConexionBd = new almacendepersonasEntities())
+            {
+                try
+                {
+                    ConexionBd.persona.Add(persona);
+                    ConexionBd.SaveChanges();
+
+                    MessageBox.Show("Se ha registrado exitosamente");
+
+                }catch(Exception ex)
+                {
+                    MessageBox.Show("Error de base de datos");
+                }
+            }
+        }
+
+        public List<persona> listOfDataOfTablePersona()
+        {
+            using (almacendepersonasEntities ConexionBd = new almacendepersonasEntities())
+            {
+                List<persona> ListPerson = ConexionBd.persona.ToList();
+                return ListPerson;
+            }
+        }
+                
     }
 }
