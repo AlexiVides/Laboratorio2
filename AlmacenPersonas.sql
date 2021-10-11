470d32a5763a7285c6934377a846c4fa9d9e5e8d
AlexiVides <88692360+AlexiVides@users.noreply.github.com>
2021-10-11 17:14:04 -0600

    Codigo SQL e Integrantes

---
+++ AlmacenPersonas.sql
@@ -0,0 +1,104 @@
+USE [master]
+GO
+/****** Object:  Database [almacendepersonas]    Script Date: 11/10/2021 17:07:35 ******/
+CREATE DATABASE [almacendepersonas]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'almacendepersonas', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\almacendepersonas.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'almacendepersonas_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\almacendepersonas_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT
+GO
+ALTER DATABASE [almacendepersonas] SET COMPATIBILITY_LEVEL = 150
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [almacendepersonas].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+ALTER DATABASE [almacendepersonas] SET ANSI_NULL_DEFAULT OFF 
+GO
+ALTER DATABASE [almacendepersonas] SET ANSI_NULLS OFF 
+GO
+ALTER DATABASE [almacendepersonas] SET ANSI_PADDING OFF 
+GO
+ALTER DATABASE [almacendepersonas] SET ANSI_WARNINGS OFF 
+GO
+ALTER DATABASE [almacendepersonas] SET ARITHABORT OFF 
+GO
+ALTER DATABASE [almacendepersonas] SET AUTO_CLOSE OFF 
+GO
+ALTER DATABASE [almacendepersonas] SET AUTO_SHRINK OFF 
+GO
+ALTER DATABASE [almacendepersonas] SET AUTO_UPDATE_STATISTICS ON 
+GO
+ALTER DATABASE [almacendepersonas] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+ALTER DATABASE [almacendepersonas] SET CURSOR_DEFAULT  GLOBAL 
+GO
+ALTER DATABASE [almacendepersonas] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+ALTER DATABASE [almacendepersonas] SET NUMERIC_ROUNDABORT OFF 
+GO
+ALTER DATABASE [almacendepersonas] SET QUOTED_IDENTIFIER OFF 
+GO
+ALTER DATABASE [almacendepersonas] SET RECURSIVE_TRIGGERS OFF 
+GO
+ALTER DATABASE [almacendepersonas] SET  ENABLE_BROKER 
+GO
+ALTER DATABASE [almacendepersonas] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+ALTER DATABASE [almacendepersonas] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+ALTER DATABASE [almacendepersonas] SET TRUSTWORTHY OFF 
+GO
+ALTER DATABASE [almacendepersonas] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+ALTER DATABASE [almacendepersonas] SET PARAMETERIZATION SIMPLE 
+GO
+ALTER DATABASE [almacendepersonas] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+ALTER DATABASE [almacendepersonas] SET HONOR_BROKER_PRIORITY OFF 
+GO
+ALTER DATABASE [almacendepersonas] SET RECOVERY FULL 
+GO
+ALTER DATABASE [almacendepersonas] SET  MULTI_USER 
+GO
+ALTER DATABASE [almacendepersonas] SET PAGE_VERIFY CHECKSUM  
+GO
+ALTER DATABASE [almacendepersonas] SET DB_CHAINING OFF 
+GO
+ALTER DATABASE [almacendepersonas] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+ALTER DATABASE [almacendepersonas] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+ALTER DATABASE [almacendepersonas] SET DELAYED_DURABILITY = DISABLED 
+GO
+ALTER DATABASE [almacendepersonas] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+EXEC sys.sp_db_vardecimal_storage_format N'almacendepersonas', N'ON'
+GO
+ALTER DATABASE [almacendepersonas] SET QUERY_STORE = OFF
+GO
+USE [almacendepersonas]
+GO
+/****** Object:  Table [dbo].[persona]    Script Date: 11/10/2021 17:07:35 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[persona](
+	[idpersona] [int] IDENTITY(1,1) NOT NULL,
+	[nombrePersona] [varchar](50) NULL,
+	[edadPersona] [int] NULL,
+	[descripcionPersona] [varchar](50) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[idpersona] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+USE [master]
+GO
+ALTER DATABASE [almacendepersonas] SET  READ_WRITE 
+GO
