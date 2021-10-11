224c7289ee78787607b8059d837b8334d5fcf90e
Nestor2021 <87671739+Nestor2021@users.noreply.github.com>
2021-10-11 15:27:49 -0600

    Capa Negocio

--- Laboratorio2/Laboratorio2/Negocio/ClsPersona.cs
+++ Laboratorio2/Laboratorio2/Negocio/ClsPersona.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using Laboratorio2.Data;
+using Laboratorio2.Reposity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Laboratorio2.Negocio
 {
-    class ClsPersona
+    class ClsPersona : ClsRPersona
     {
+        public void Create(persona per)
+        {
+            if (per.edadPersona >= 18)
+            {
+                Guardar(per);
+            }
+            else
+            {
+                MessageBox.Show("No es mayor de edad", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+
+        public List<persona> ListMostrar()
+        {
+            List<persona> list = listOfDataOfTablePersona();
+            return list;
+        }
+
+
     }
 }
